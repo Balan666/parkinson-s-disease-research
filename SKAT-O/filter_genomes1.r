b9b11b96f9df8c567c865ee375ba0228258e8f7e
Balan666 <113861516+Balan666@users.noreply.github.com>
2025-05-21 01:44:34 +0300

    Update filter_genomes1.r

--- SKAT-O/filter_genomes1.r
+++ SKAT-O/filter_genomes1.r
@@ -2,7 +2,7 @@
 
 library(data.table)
 
-prs_data <- fread("~/runs/senkkon/2025/AMP_PD_PRS/SKAT-O_run_ver1/AMP_PD_plink_ver2_res.0.4.profile")
+prs_data <- fread("../AMP_PD_plink_ver2_res.0.4.profile")
 
 # Sort by PRS scores
 prs_data <- prs_data[order(SCORE)]
@@ -12,5 +12,5 @@ n <- nrow(prs_data)
 top_25 <- prs_data[(0.75 * n):n, ]
 bottom_25 <- prs_data[1:(0.25 * n), ]
 
-write.table(top_25[, .(FID, IID)], "~/runs/senkkon/2025/AMP_PD_PRS/SKAT-O_run_ver1/inter_files/PRS_top_25_fids.txt", col.names = FALSE, row.names = FALSE, sep = "\t")
-write.table(bottom_25[, .(FID, IID)], "~/runs/senkkon/2025/AMP_PD_PRS/SKAT-O_run_ver1/inter_files/PRS_bottom_25_fids.txt", col.names = FALSE, row.names = FALSE, sep = "\t")
+write.table(top_25[, .(FID, IID)], "./inter_files/PRS_top_25_fids.txt", col.names = FALSE, row.names = FALSE, sep = "\t")
+write.table(bottom_25[, .(FID, IID)], "./inter_files/PRS_bottom_25_fids.txt", col.names = FALSE, row.names = FALSE, sep = "\t")
